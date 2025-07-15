19445b7cff8ba8ab76f7c8da42b0e51deff69b79
wattttta <watta1027@gmail.com>
2025-07-15 12:36:15 +0900

    test

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from domain.constants import RSI_THRESHOLD
 from application.rsi_notify_service import notify_rsi_signal
 from application.get_stocks_service import get_stocks
 
-# DBから株の情報を取得する
-stocks = get_stocks()
+def main(request):
+    # DBから株の情報を取得する
+    stocks = get_stocks()
 
-# RSIの閾値を超えた株の情報を通知する
-notify_rsi_signal(stocks, RSI_THRESHOLD)
+    # RSIの閾値を超えた株の情報を通知する
+    notify_rsi_signal(stocks, RSI_THRESHOLD)
